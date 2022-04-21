89359fd809b8921681d3298df3c76175d814a5c6
Sasha Jenner <jenner.sasha@gmail.com>
2022-04-21 14:51:34 +1000

    adding plotly

--- viz/trally.R
+++ viz/trally.R
@@ -15,11 +15,24 @@ if (length(args) != 1) {
 path = args[1]
 df = read.delim(path, sep='\t')
 name = basename(path)
-colname = names(df)[1]
+colnames = names(df)
 title = paste(name, 'Transition Matrix')
-xtitle = colname
+xtitle = colnames[1]
+ytitle = colnames[2]
 
-plot = ggplot(df, aes(x=to, y=from, colour=freq, fill=freq)) +
+plot = ggplot(df, aes(x=from, y=to, colour=freq, fill=freq)) +
 	geom_tile() +
+	#geom_abline(slope=1) +
 	labs(title = title)
 ggsave(paste0(path, '.trix.pdf'), plot)
+
+from = unique(df[,1])
+side = length(from)
+dfm = matrix(df[,3], nrow=side, ncol=side)
+
+plotly = plot_ly(x=from, y=from, z=dfm,
+		 type = 'heatmap',
+		 name = title) %>%
+	layout(xaxis = list(title = xtitle),
+		yaxis = list(title = ytitle))
+saveWidget(plotly, paste0(path, '.trix.html'))
